214ded946051f02ca0829cf26b75fcbfe4f6db03
Fabianko <fabian.arismendi@usach.cl>
2021-04-24 15:37:02 -0400

    ADD: components and style

--- spotify-angular/src/app/app.module.ts
+++ spotify-angular/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SongComponent } from './song/song.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SongComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
